011bc6730906e15efcae565e7cd91a427782460e
andreigrig-oras <andreigrigoras@gmail.com>
2026-06-03 19:41:20 +1000

    last commit of the day - completed the chapter 18 app

--- django/learning_log/learning_logs/views.py
+++ django/learning_log/learning_logs/views.py
@@ -13,6 +13,6 @@ def topics (request):
 def topic (request, topic_id):
     #Show a sinle topic and all it's entries
     topic = Topic.objects.get(id=topic_id)
-    entries=topic.entry_set.order_by('-date added')
+    entries=topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
